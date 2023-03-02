e4e36bef8e658b16b1796b5d7bf6e586c781dba1
wxd <2290094258@qq.com>
2023-03-02 21:17:52 +0800

    start to learning multiple threads

---
+++ multi_thread/MT.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 王旭东 on 2023/3/2.
+//
+#include "MT.H"
+
+void mt_test(){
+
+}
---
+++ multi_thread/MT.h
@@ -0,0 +1,8 @@
+//
+// Created by 王旭东 on 2023/3/2.
+//
+
+#ifndef MUTI_THREADS_MT_H
+#define MUTI_THREADS_MT_H
+void mt_test();
+#endif //MUTI_THREADS_MT_H
